bd94c4860f65aa0fe33e1c222f40999363c4d3e4
AJ Harrington <r11132a@gmail.com>
2019-07-09 15:09:27 -0400

    Multiple 'builds' per travis setup.
    Running tests with both premium and non-premium keys on travis

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,14 @@
 # Test config
 use Mix.Config
 
+fixup_premium = fn val ->
+  case val do
+    "false" -> false
+    "0" -> false
+    _ -> true
+  end
+end
+
 config :ex_timezone_db, api_key: System.get_env("TIMEZONE_DB_API_KEY")
+config :ex_timezone_db, premium: fixup_premium.(System.get_env("TIMEZONE_DB_PREMIUM","false"))
+
